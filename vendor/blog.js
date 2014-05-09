d9e3006e703dfc7cfe957d13002876fcf6045024
fritx <uxfritz@163.com>
2014-05-09 20:08:23 +0800

    add onNotFound

--- vendor/blog.js
+++ vendor/blog.js
@@ -30,71 +30,79 @@
   function load(sel, page, isMain, options, callback) {
     isMain = isMain || false;
     var url = pageBase + page + '.' + pageExt;
-    $.get(url, function (data) {
-      render(data, options, function (err, html) {
-        if (err && callback) return callback(err);
-        var $el = $(sel);
-        $el.hide().html(html);
+    $.ajax({
+      url: url,
+      error: onNotFound,
+      success: function (data) {
+        render(data, options, function (err, html) {
+          if (err && callback) return callback(err);
+          var $el = $(sel);
+          $el.hide().html(html);
 
-        $el.find('[src]').each(function () {
-          var $el = $(this);
-          $el.attr('src', function (x, old) {
-            if (isAbsolute(old)) {
-              return old;
-            }
-            return url.replace(
-              new RegExp('[^\\/]*$', 'g'), ''
-            ) + old;
-          });
-        });
-
-        $el.find('[href]').each(function () {
-          var $el = $(this);
-          $el.attr('href', function (x, old) {
-            if (isAbsolute(old)) {
-              $el.attr('target', '_blank');
-              return old;
-            }
-            var prefixed = url.replace(
-              new RegExp('^' + pageBase + '|[^\\/]*$', 'g'), ''
-            ) + old;
-            var regExt = new RegExp('\\.' + pageExt + '$');
-            if (!regExt.test(old)) {
-              if (!/(^\.|\/\.?|\.html?)$/.test(old)) {
-                $el.attr('target', '_blank');
+          $el.find('[src]').each(function () {
+            var $el = $(this);
+            $el.attr('src', function (x, old) {
+              if (isAbsolute(old)) {
+                return old;
               }
-              return prefixed;
-            }
-            return '?' + prefixed.replace(regExt, '');
+              return url.replace(
+                new RegExp('[^\\/]*$', 'g'), ''
+              ) + old;
+            });
           });
+
+          $el.find('[href]').each(function () {
+            var $el = $(this);
+            $el.attr('href', function (x, old) {
+              if (isAbsolute(old)) {
+                $el.attr('target', '_blank');
+                return old;
+              }
+              var prefixed = url.replace(
+                new RegExp('^' + pageBase + '|[^\\/]*$', 'g'), ''
+              ) + old;
+              var regExt = new RegExp('\\.' + pageExt + '$');
+              if (!regExt.test(old)) {
+                if (!/(^\.|\/\.?|\.html?)$/.test(old)) {
+                  $el.attr('target', '_blank');
+                }
+                return prefixed;
+              }
+              return '?' + prefixed.replace(regExt, '');
+            });
+          });
+
+          if (isMain) {
+            mainTitle = $el.find('h1:first').text();
+            $('title').text(function (x, old) {
+              return mainTitle + ' - ' + old;
+            });
+
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            window.disqus_shortname = 'fritx'; // required: replace example with your forum shortname
+            window.disqus_title = mainTitle;
+            window.disqus_identifier = mainPage;
+            window.disqus_url = location.href.replace(/#.*$/, '');
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function () {
+              var dsq = document.createElement('script');
+              dsq.type = 'text/javascript';
+              dsq.async = true;
+              dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+              (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+          }
+
+          $el.show();
+          if (callback) callback();
         });
-
-        if (isMain) {
-          mainTitle = $el.find('h1:first').text();
-          $('title').text(function (x, old) {
-            return mainTitle + ' - ' + old;
-          });
-
-          /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-          window.disqus_shortname = 'fritx'; // required: replace example with your forum shortname
-          window.disqus_title = mainTitle;
-          window.disqus_identifier = mainPage;
-          window.disqus_url = location.href.replace(/#.*$/, '');
-
-          /* * * DON'T EDIT BELOW THIS LINE * * */
-          (function () {
-            var dsq = document.createElement('script');
-            dsq.type = 'text/javascript';
-            dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-          })();
-        }
-
-        $el.show();
-        if (callback) callback();
-      });
+      }
     });
+  }
+
+  function onNotFound() {
+    location.href = '.';
   }
 
   function start() {
