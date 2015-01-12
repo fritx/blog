ae39b364f2921fd53525117e6331f4af38a876bb
Fritz Lin <uxfritz@163.com>
2015-01-12 15:25:55 +0800

    hack for wxtimeline

--- vendor/blog.js
+++ vendor/blog.js
@@ -200,10 +200,14 @@
       }
     }
     wx.ready(function(){
-      wx.onMenuShareTimeline(wxData);
-      wx.onMenuShareAppMessage(wxData);
-      wx.onMenuShareQQ(wxData);
-      wx.onMenuShareWeibo(wxData);
+      wx.onMenuShareAppMessage(wxData)
+      wx.onMenuShareQQ(wxData)
+      wx.onMenuShareWeibo(wxData)
+      // hack for timeline
+      wx.onMenuShareTimeline($.extend({}, wxData, {
+        title: document.title
+      }))
+      wx.showOptionMenu()
     })
   }
 
@@ -240,6 +244,9 @@
     pageBase = 'p/';
     defaultPage = 'tech';
 
+    wx.ready(function(){
+      wx.hideOptionMenu()
+    })
     wx.config({
       debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       //appId: '', // 必填，公众号的唯一标识
@@ -247,7 +254,7 @@
       //nonceStr: '', // 必填，生成签名的随机串
       //signature: '',// 必填，签名，见附录1
       jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    });
+    })
   }
 
 })();
