b0903e6d6aca66bf6de21b05d71e94209b819c1c
Fritz Lin <uxfritz@163.com>
2015-01-12 15:07:44 +0800

    switch to official jweixin-1.0.0

--- vendor/blog.js
+++ vendor/blog.js
@@ -185,22 +185,26 @@
   }
 
   function shares() {
-    var wxData = function() {
-      return {
-        // 这里需要特别说明的是，建议不要用新浪微博的图片地址，要么你试试，哈哈
-        'img': entryUrl + $('#img-holder').attr('src'),
-        'link': location.href,
-        'desc': $('#main-page').find('h1').text(),
-        'title': $('#sidebar-page').find('h1').text()
-      };
-    };
-    wechat('friend', wxData, wxCallback);     // 好友
-    wechat('timeline', wxData, wxCallback);   // 朋友圈
-    wechat('weibo', wxData, wxCallback);      // 微博
-
-    function wxCallback(res) {
-      console.log(JSON.stringify(res))
+    var wxData = {
+      title: $('#sidebar-page').find('h1').text(), // 分享标题
+      desc: $('#main-page').find('h1').text(), // 分享描述
+      link: location.href, // 分享链接
+      imgUrl: entryUrl + $('#img-holder').attr('src'), // 分享图标
+      //type: '', // 分享类型,music、video或link，不填默认为link
+      //dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+      success: function (){ 
+        // 用户确认分享后执行的回调函数
+      },
+      cancel: function (){ 
+        // 用户取消分享后执行的回调函数
+      }
     }
+    wx.ready(function(){
+      wx.onMenuShareTimeline(wxData);
+      wx.onMenuShareAppMessage(wxData);
+      wx.onMenuShareQQ(wxData);
+      wx.onMenuShareWeibo(wxData);
+    })
   }
 
   function start() {
@@ -235,6 +239,15 @@
     pageExt = '.md';
     pageBase = 'p/';
     defaultPage = 'tech';
+
+    wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      //appId: '', // 必填，公众号的唯一标识
+      timestamp: new Date().getTime(), // 必填，生成签名的时间戳
+      //nonceStr: '', // 必填，生成签名的随机串
+      //signature: '',// 必填，签名，见附录1
+      jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    });
   }
 
 })();
